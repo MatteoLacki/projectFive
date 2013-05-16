f9a321faa0d720ce48e26fb8c18164dbabb1533d
Michal Derezinski <michal@eduroam-10-20-4-250.mimuw.edu.pl>
2013-05-16 13:46:57 +0200

    Pro

--- scripts/Introduction.R
+++ scripts/Introduction.R
@@ -1,2 +1,2 @@
-# Hello and welcome to project 5. We're going to speak about tobacco and get some lolly out of the poppy. 
+# Hello and welcome to project 5. We're going to speak about tobacco and get some lolly out of the poppy!
 
