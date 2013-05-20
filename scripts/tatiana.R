13418e1fc97a006dcc32adf936f28ea846272806
Tiziana Vyrtum <ryabukha@mimuw.edu.pl>
2013-05-20 17:57:36 +0200

    qplot dziala ale sa duze liczby.
    ggplot niedzial - no layer data.

--- scripts/tatiana.R
+++ scripts/tatiana.R
@@ -1,2 +1,22 @@
 library(ggplot2)
 library(plyr)
+
+attach(Data)
+
+
+
+
+qplot(Income, ..density.., data=Data, geom="density", fill=Smoker_Group, position="stack") +
+   coord_flip()
+
+
+
+
+
+ggplot(data=Data,aes(x=Income,fill=Smoker_Group)) + 
+  geom_bar(subset=.(Gender=="F")) + 
+  geom_bar(subset=.(Gender=="M"),aes(y=..count..*(-1))) + 
+  scale_y_continuous(breaks=seq(-40,40,10),labels=abs(seq(-40,40,10))) + 
+  coord_flip()
+
+summary(Income)
