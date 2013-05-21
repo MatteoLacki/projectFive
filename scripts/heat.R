aeeca8f40f423ee4207923035d8286ebe4d713ae
lr292358 <lukasz21212121@wp.pl>
2013-05-21 21:26:21 +0200

    heat poprawiona

--- scripts/heat.R
+++ scripts/heat.R
@@ -4,54 +4,62 @@ library(plyr)
 
 # select
 names <- c("Smokes",
-           "Daily_Smokes",
            "Marital_Status",
-           "Age",
-           "Income"
+           "Age_Group",
+           "Smoker_Group"
 ) 
 
 # read data from Cleanup.r and clean
 hdata <- Data[,names]
-hdata <- hdata[!is.na(hdata$Age) & !is.na(hdata$Marital_Status),]
-
-# income if they are a smoker or NA
-incomeSmokerOrNA <- function(a, b){
-  if(b == 'yes')
-    a else NA
-}
-
-# label: marital status + age
-getLabel <- function(text, under40){
-  if(under40){
-    a <- ", under 40" 
-  } else {
-    a <- ", 40+"
-  }
-  paste(text,a) 
-}
-
-# group by (marital status, age) and aggregate results
-hdata2 = ddply(hdata, ~ Marital_Status ~ Age < 40,
-               summarise,                
-               meanIncome = mean(Income, na.rm=TRUE), 
-               meanIncomeSmokers = 
-                 mean(mapply(incomeSmokerOrNA, Income, Smokes), na.rm=TRUE), 
-               smokers = sum(Smokes == 'yes')/length(Smokes), 
-               numberOfCiggarets = 
-                  sum(Daily_Smokes)/length(Smokes))
-         )
-
-# create labels
-rownames(hdata2) <- mapply(getLabel, hdata2[,1], hdata2[,2]) 
-hdata2[,1] <- NULL
-hdata2[,1] <- NULL
+hdata <- hdata[!is.na(hdata$Age_Group) & !is.na(hdata$Smoker_Group) & !is.na(hdata$Marital_Status),]
+# join divorced and separated
+hdata$Marital_Status <- factor(hdata$Marital_Status, 
+                      levels = c(levels(hdata$Marital_Status), "divorced/sep."))
+hdata$Marital_Status[hdata$Marital_Status == 'separated'] <- 'divorced/sep.'
+hdata$Marital_Status[hdata$Marital_Status == 'divorced'] <- 'divorced/sep.'
+# join 60+ into one group
+hdata$Age_Group <- factor(hdata$Age_Group, 
+                          levels = c(levels(hdata$Age_Group), "0-34"))
+hdata$Age_Group[hdata$Age_Group == '0-24'] <- '0-34'
+hdata$Age_Group[hdata$Age_Group == '25-34'] <- '0-34'
+hdata$Age_Group <- factor(hdata$Age_Group, 
+                          levels = c(levels(hdata$Age_Group), "60+"))
+hdata$Age_Group[hdata$Age_Group == '60-64'] <- '60+'
+hdata$Age_Group[hdata$Age_Group == '65+'] <- '60+'
+hdata$Age_Group <- factor(hdata$Age_Group)
 
 
+for(i in levels(hdata$Age_Group)){
+  png(file = sprintf("heatPlot_%s.png",i),res=30,
+       pointsize=50, width=800, height=500)
+  
+  hdata1 <- hdata[hdata$Age_Group ==i,]
+  # plot by Age_group: 
+  # group by marital status and aggregate results
+  hdata2 = ddply(hdata1, ~ Marital_Status,
+                 summarise,          
+                 more_than_one_pack = sum(Smoker_Group == 'more than one pack' )
+                   /length(Smoker_Group == 'more than one pack' ),
+                 up_to_one_a_pack = sum(Smoker_Group == 'up to one pack' )
+                   /length(Smoker_Group == 'up to one pack' ),
+                 up_to_half_a_pack = sum(Smoker_Group == 'up to half a pack' )
+                   /length(Smoker_Group == 'up to half a pack' ),
+                 former_smoker = sum(Smoker_Group == 'former smoker' )
+                   /length(Smoker_Group == 'former smoker' ),
+                 never_smoked = sum(Smoker_Group == 'never smoked' )
+                   /length(Smoker_Group == 'never smoked' )
+ )
+  
+  
+  #create heatmap
+  rownames(hdata2) <- hdata2[,1]
+  hdata2[,1] <- NULL
+  xh <- as.matrix(hdata2)
+  heatmap(t(-xh), Rowv = NA, Colv = NA, scale = "row", cexRow=1.1, cexCol=1.1,  margins = c(9, 11),  labRow = gsub("_", " ", rownames(t(-xh))), labCol = c('single','married','widowed','divorced or\n separated '))
+  dev.off()
+} 
 
-#create heatmap
-xh <- as.matrix(hdata2)
-heatmap(t(-xh), Rowv = NA, Colv = NA, scale = "row", cexRow=1.1, cexCol=1.1, 
-        margins = c(10, 16))
+
 
 
 
